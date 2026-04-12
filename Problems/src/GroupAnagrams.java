cc909eea03647e0d3f889d1701ae737b114c5f8f
Parsa Shadkam <parsashadkam2017@gmail.com>
2026-04-12 13:54:39 +0330

    hash table question 49

---
+++ Problems/src/GroupAnagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+
+        for (String s:strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key,k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}

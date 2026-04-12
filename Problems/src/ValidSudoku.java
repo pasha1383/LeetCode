3fa1dbc2f811d0f9019138a6f89d1a271ef73d86
Parsa Shadkam <parsashadkam2017@gmail.com>
2026-04-12 22:08:39 +0330

    hash table question 36

---
+++ Problems/src/ValidSudoku.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ValidSudoku {
+    public static void main(String[] args) {
+        char[][] board = {
+                {},
+                {},
+        };
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char number = board[i][j];
+                if (number != '.') {
+                    if ( !seen.add(number + " in row " + i) ||
+                         !seen.add(number + " in column " + j) ||
+                         !seen.add(number + " in box " + i/3 + "-" +j/3 )
+                    ){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}

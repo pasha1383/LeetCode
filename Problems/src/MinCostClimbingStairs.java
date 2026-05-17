3c8f9150099e2277d8166dbdb63b4d74af915116
Parsa Shadkam <parsashadkam2017@gmail.com>
2026-05-17 14:36:09 +0330

    solve #746 LeetCode second approach

--- Problems/src/MinCostClimbingStairs.java
+++ Problems/src/MinCostClimbingStairs.java
@@ -1,13 +1,24 @@
 import java.util.Arrays;
 
 public class MinCostClimbingStairs {
+//    public static int minCostClimbingStairs(int[] cost) {
+//        int n = cost.length;
+//        int[] dp = new int[n+1];
+//        for (int i = 2; i <= n ; i++) {
+//            dp[i] = Math.min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2]);
+//        }
+//        return dp[n];
+//    }
+
     public static int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n+1];
+        int prev2=0,prev1= 0;
         for (int i = 2; i <= n ; i++) {
-            dp[i] = Math.min(dp[i-1] + cost[i-1],dp[i-2] + cost[i-2]);
+            int curr = Math.min(prev1 + cost[i-1],prev2 + cost[i-2]);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
 
     public static void main(String[] args) {

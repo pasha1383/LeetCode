b1e69c8bbead452c218054cdf6c76c439dc608ac
Parsa Shadkam <parsashadkam2017@gmail.com>
2026-05-17 14:22:18 +0330

    solve #509 LeetCode second approach

--- Problems/src/FibonacciNumber.java
+++ Problems/src/FibonacciNumber.java
@@ -1,21 +1,33 @@
 import java.util.Arrays;
 
 public class FibonacciNumber {
+//    public static int fib(int n) {
+//        int[] dp = new int[n+1];
+//        if (n == 0) {
+//            return 0;
+//        }
+//        dp[0] = 0;
+//        dp[1] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+////        System.out.println(Arrays.toString(dp));
+//        return dp[n];
+//    }
+
     public static int fib(int n) {
-        int[] dp = new int[n+1];
-        if (n == 0) {
-            return 0;
-        }
-        dp[0] = 0;
-        dp[1] = 1;
+        if (n == 0 || n == 1) return n;
+        int prev2 = 0,prev1 = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-//        System.out.println(Arrays.toString(dp));
-        return dp[n];
+
+        return prev1;
     }
 
     public static void main(String[] args) {
-        System.out.println(fib(1));
+        System.out.println(fib(10));
     }
 }

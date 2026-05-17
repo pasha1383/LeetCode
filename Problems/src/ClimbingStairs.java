9ca095c2711a1928cf7d83688db5a571d466350c
Parsa Shadkam <parsashadkam2017@gmail.com>
2026-05-17 14:07:15 +0330

    solve #70 LeetCode second approach

--- Problems/src/ClimbingStairs.java
+++ Problems/src/ClimbingStairs.java
@@ -1,19 +1,31 @@
 import java.util.Arrays;
 
 public class ClimbingStairs {
-    public static int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+//    public static int climbStairs(int n) {
+//        int[] dp = new int[n + 1];
+//        dp[0] = 1;
+//        dp[1] = 1;
+//
+//        for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+//        return dp[n];
+//    }
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+    public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        int prev2 = 1,prev1 = 1;
+
+        for (int i = 2; i <=n ; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return prev1;
     }
 
     public static void main(String[] args) {
-        System.out.println(climbStairs(1));
+        System.out.println(climbStairs(45));
     }
 
 
